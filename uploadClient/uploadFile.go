5e7857e59ffd8ce6a9c1b920271356d1fcfa17a0
chyuhung <977599667@qq.com>
2022-10-11 11:16:00 +0800

    命令行参数

--- uploadClient/uploadFile.go
+++ uploadClient/uploadFile.go
@@ -8,16 +8,18 @@ import (
 
 func main() {
 	var (
-		user       string // upload user
-		filePath   string // "/root/go/src/fileServer/uploadClient/files/123.txt"
-		uploadPath string // "myfile"
-		targetUrl  string // "http://127.0.0.1:27149/fileServer" "http://10.191.22.9:8001/27149"
+		user       string // 上传用户名
+		filePath   string // 本地文件路径
+		uploadPath string // 指定远端文件目录
+		targetUrl  string // 服务端URL 测试：http://127.0.0.1:27149/fileServer http://10.191.22.9:8001/27149
+		isCover    bool
 	)
 	//命令行参数
-	flag.StringVar(&user, "u", "default", "upload user.")
+	flag.StringVar(&user, "u", "public", "upload user.")
 	flag.StringVar(&filePath, "f", "", "file path.")
-	flag.StringVar(&uploadPath, "d", "", "remote dir path.")
+	flag.StringVar(&uploadPath, "d", "/*home*", "remote dir path. option: /*home*, /*public*.\neg: -d /*home*/newdir.\n")
 	flag.StringVar(&targetUrl, "l", "http://127.0.0.1:27149/fileServer", "server url.")
+	flag.BoolVar(&isCover, "o", false, "overwrite")
 	flag.Parse()
 
 	uploadModel := model.UploadModel{}
@@ -28,7 +30,7 @@ func main() {
 	}
 	// set url
 	uploadModel.SetUrl(targetUrl)
-	uploadModel.IsCover = false //是否覆盖上传
+	uploadModel.IsCover = isCover //是否覆盖上传
 	//fileHash := arsHash.FileHash(filePath)
 	//fmt.Println(fileHash)
 
@@ -41,14 +43,15 @@ func main() {
 	fmt.Printf("获取上传进度(缓存文件大小): %d\n", progressInfo.Progress)
 
 	if len(progressInfo.FileInfoList) > 0 {
-		fmt.Printf("服务端新文件名: %s\n", progressInfo.NewName)
+		fmt.Printf("服务端预定义文件名: %s\n", progressInfo.NewName)
 		i := 0
 		for i < len(progressInfo.FileInfoList) {
 			item := progressInfo.FileInfoList[i]
-			fmt.Printf("远端路径有重名文件\n")
+			fmt.Printf("\n远端路径有重名文件\n")
+			fmt.Printf("文件名称: %s\n", item.FileName)
 			fmt.Printf("文件大小: %d\n", item.FileSize)
 			fmt.Printf("文件哈希: %s\n", item.FileHash)
-			fmt.Printf("文件名称: %s\n", item.FileName)
+			fmt.Printf("\n")
 			i++
 		}
 
